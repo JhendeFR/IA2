45ee882a28124ce2115920a6b424f85995bff979
JhendeFR <jheandef@gmail.com>
2025-09-11 20:39:06 -0400

    prueba11/9

--- 1er Parcial/ACTAS/pruebarecorte.py
+++ 1er Parcial/ACTAS/pruebarecorte.py
@@ -1,25 +1,18 @@
 import cv2
 import os
 
-# ================== CONFIG ==================
-img_path = r"C:\Users\jhean\Documents\Universidad\IA2\1er Parcial\ACTAS\imagenes_unificadas\000001_mesa_3_8004251.jpg"   # ruta a tu imagen original del acta
-output_dir = "Mesas"                     # carpeta de salida
-output_name = "votos.jpg"         # nombre del archivo recortado
-# ============================================
+img_path = r"C:\Users\jhean\Documents\Universidad\IA2\1er Parcial\ACTAS\imagenes_unificadas\000001_mesa_3_8004251.jpg"
+output_dir = "Mesas"
+output_name = "votos.jpg"
 
-# Crear carpeta destino
 os.makedirs(output_dir, exist_ok=True)
 
-# Leer imagen
+#Leer la imagen
 img = cv2.imread(img_path)
+#Configuracion de recorte
+x, y, w, h = 500, 600, 800, 1200 
 
-# --- Coordenadas de recorte ---
-# Como punto de partida: define manualmente (x, y, w, h)
-# Estos valores funcionan para tu acta de ejemplo (ajústalos según necesites)
-# En la imagen de 3336x2200 el bloque "MESA" está aprox. en la esquina izquierda.
-x, y, w, h = 500, 600, 800, 1200   # <--- AJUSTA ESTOS VALORES
-
-# Recortar
+#Recortar
 mesa_crop = img[y:y+h, x:x+w]
 
 # Guardar
